57cf6818b987cf072a8baa83d9f27b403979283b
Reinald Reynoso <reireynoso@gmail.com>
2019-03-14 01:01:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,9 +88,9 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.collect do |holi, items|
       #holi is a string #items array
-        holi = holi.to_s
-        holi.split("_")
-        puts "  #{holi.capitalize}: #{items.join(", ")}"
+       # holi = holi.to_s
+        #holi.split("_")
+        #puts "  #{holi.capitalize}: #{items.join(", ")}"
         
     end
     
